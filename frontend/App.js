d935f0619b537761f387a33e056c9424f1d7f813
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-02 15:45:57 -0300

    Initial frontend (React) structure.

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>Rehearsal Scheduler</h1>
+      <p>Welcome to the Rehearsal Scheduler app. Login to get started.</p>
+    </div>
+  );
+}
+export default App;
